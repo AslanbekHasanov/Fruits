1bed8d9782fa61040f2f3aea4a6e7f5fd47b9cb2
AslanbekHasanov <learneraslanbek1220@gmail.com>
2024-02-29 12:04:58 +0500

    Write Bussines Logic

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+
+using Fruits.Service;
+
+Fruit apple = new Fruit();
+
+apple.name = "Golden";
+apple.color = "Yellow";
+
+System.Console.WriteLine($"Fruit is name: {apple.Name}\nFruit is color {apple.Color}");
---
+++ Service/Fruit.cs
@@ -0,0 +1,32 @@
+// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE TO CONNECT THE WORLD
+// ---------------------------------------------------------------
+namespace Fruits.Service
+{
+    public class Fruit
+    {
+
+        public string name = "olma";
+        public string color = "green";
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+        }
+        public string Color
+        {
+            get
+            {
+                return color;
+
+            }
+
+        }
+
+
+    }
+}
